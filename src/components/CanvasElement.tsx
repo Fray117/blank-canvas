f0b021404477788a864e82f2c4c2282e815840f4
Fray Dalton <fray117@exploiter.id>
2023-06-19 21:31:56 +0700

    add input type

--- src/components/CanvasElement.tsx
+++ src/components/CanvasElement.tsx
@@ -3,7 +3,7 @@ import { Dispatch, SetStateAction } from 'react'
 export default function CanvasElement({ data }: { data: { shape: boolean, class: string } }) {
 	return (
 		<>
-			{data.shape ? <div className={data.class} ></div> : <input className={data.class} autoFocus />}
+			{data.shape ? <div className={data.class} ></div> : <input type="text" className={data.class} autoFocus />}
 		</>
 	)
 }
