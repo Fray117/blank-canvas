a2ed865c327dd18ca5320740b4571a83dbde1234
Fray Dalton <fray117@exploiter.id>
2023-06-20 12:39:45 +0700

    cleanup

--- src/components/CanvasElement.tsx
+++ src/components/CanvasElement.tsx
@@ -30,15 +30,9 @@ export default function CanvasElement({ data, onStart, onStop, position }: {
 		<Draggable onStart={onStart} onStop={onStop} defaultPosition={position} nodeRef={elementRef} cancel=".react-resizable-handle">
 			<Resizable onResize={resizableHandler} height={height} width={width} resizeHandles={['sw', 'se', 'nw', 'ne', 'w', 'e', 'n', 's']}>
 				<div className="box fixed" ref={elementRef} style={{ width: width + 'px', height: height + 'px' }}>
-					{data.source === '' ? data.shape ? <div className={data.class} ></div> : <input type="text" className={data.class} autoFocus /> : <Image draggable="false" src={data.source} width={300} height={300} alt={''} />}
+					{data.source === '' ? data.shape ? <div className={data.class} ></div> : <input type="text" className={data.class} autoFocus /> : <Image draggable="false" src={data.source} width={width} height={height} alt={''} />}
 				</div>
 			</Resizable>
 		</Draggable>
-
-		// <Resizable>
-		// 	<div className="fixed">
-		// 		{data.source === '' ? data.shape ? <div className={data.class} ></div> : <input type="text" className={data.class} autoFocus /> : <Image draggable="false" src={data.source} width={300} height={300} alt={''} />}
-		// 	</div>
-		// </Resizable>
 	)
 }
